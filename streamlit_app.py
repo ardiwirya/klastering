8ea064bfffa78d24514333c1fa1016020b8afbed
Ardi Wirya <132697484+ardiwirya@users.noreply.github.com>
2025-08-09 17:12:56 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,6 +21,22 @@ st.markdown("""
 """)
 
 # --- Data Dummy (Ganti dengan data dari SQL Anda) ---
+curl "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent" \
+  -H 'Content-Type: application/json' \
+  -H 'X-goog-api-key: AIzaSyC71s3vAu1r28bS2tBW24ybibTIZNmh3qo' \
+  -X POST \
+  -d '{
+    "contents": [
+      {
+        "parts": [
+          {
+            "text": "Explain how AI works in a few words"
+          }
+        ]
+      }
+    ]
+  }'
+
 # DataFrame ini berfungsi sebagai pengganti data dari database SQL
 data_kecamatan = {
     'name': ["Samalanga", "Sp. Mamplam", "Pandrah", "Jeunieb", "Peulimbang", "Peudada", "Juli", "Jeumpa", "Kota Juang", "Kuala", "Jangka", "Peusangan", "Peusangan Selatan", "Peusangan Sb Krueng", "Makmur", "Gandapura", "Kuta Blang"],
